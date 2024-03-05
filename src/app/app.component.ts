5f7932da939da1e8dcf0d1985c54acc1461d4950
Uriel Blanco <uriel.blanco@contractors.roche.com>
2024-03-05 18:03:13 +0100

    feat: add one-kit table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OneWebComponentsAngularModule } from '@one/angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, OneWebComponentsAngularModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'my-app';
